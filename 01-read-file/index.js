c94339e0dfa3fc2be0a78925d3d77b98bb9c2601
Oleg Nykytenko <kitgelo@gmail.com>
2024-01-21 17:37:02 +0200

    feat: complete Reading a File with Console Output

---
+++ 01-read-file/index.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+const path = require('path');
+const readStream = fs.createReadStream(
+  path.join(__dirname, 'text.txt'),
+  'utf8',
+);
+readStream.on('data', function (data) {
+  console.log(data);
+});
